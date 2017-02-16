4db28e260e5eb669380074e135b473440457ef9e
RedDevil91 <b.attila91@gmail.com>
2017-02-16 21:31:32 +0100

    Draw values into the table

--- table.py
+++ table.py
@@ -1,14 +1,17 @@
 from PySide import QtGui, QtCore
+import numpy as np
 
 
 class Table(QtGui.QWidget):
     padding = 10
 
-    def __init__(self):
+    def __init__(self, init_values):
         super(Table, self).__init__()
         self.setWindowTitle("Sudoku Table")
         self.setGeometry(100, 0, 300, 300)
         self.setFixedSize(300, 300)
+
+        self.values = init_values
         return
 
     def paintEvent(self, event):
@@ -33,17 +36,31 @@ class Table(QtGui.QWidget):
         painter.setPen(pen)
         for i in xrange(9):
             for j in xrange(9):
-                painter.drawRect(QtCore.QRect(i * main_rect.width() / 9 + self.padding,
-                                              j * main_rect.height() / 9 + self.padding,
-                                              main_rect.width() / 9,
-                                              main_rect.height() / 9))
+                rect = QtCore.QRect(i * main_rect.width() / 9 + self.padding,
+                                    j * main_rect.height() / 9 + self.padding,
+                                    main_rect.width() / 9,
+                                    main_rect.height() / 9)
+                painter.drawRect(rect)
+                if self.values[j, i] != 0:
+                    painter.drawText(rect, QtCore.Qt.AlignCenter, str(self.values[j, i]))
         painter.end()
         return
 
 
 if __name__ == '__main__':
     import sys
+
+    puzzle2 = np.array([[0, 8, 0, 1, 0, 0, 0, 0, 0],
+                        [6, 0, 0, 0, 0, 4, 8, 0, 0],
+                        [3, 5, 4, 2, 6, 0, 7, 9, 1],
+                        [2, 9, 3, 0, 8, 0, 0, 0, 4],
+                        [0, 0, 0, 7, 0, 9, 0, 0, 0],
+                        [7, 0, 0, 0, 4, 0, 9, 6, 5],
+                        [8, 2, 9, 0, 5, 7, 6, 1, 3],
+                        [0, 0, 7, 8, 0, 0, 0, 0, 9],
+                        [0, 0, 0, 0, 0, 6, 0, 8, 0]])
+
     app = QtGui.QApplication(sys.argv)
-    table = Table()
+    table = Table(puzzle2)
     table.show()
     sys.exit(app.exec_())

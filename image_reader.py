19844e6224ffb4e2e2b86acecd9f41f71226a567
RedDevil91 <b.attila91@gmail.com>
2017-02-18 15:27:03 +0100

    ImageProcessor class with image preprocessing

--- image_reader.py
+++ image_reader.py
@@ -2,32 +2,55 @@ import cv2
 import sys
 import numpy as np
 
-kernel = np.array([[0, 1, 0],
-                   [1, 1, 1],
-                   [0, 1, 0]], dtype=np.uint8)
 
-cap = cv2.VideoCapture(0)
+class ImageProcessor(object):
+    kernel = np.array([[0, 1, 0],
+                       [1, 1, 1],
+                       [0, 1, 0]], dtype=np.uint8)
 
-if not cap.isOpened():
-    print "Failed to open caption!"
-    sys.exit(1)
+    def __init__(self):
+        self.raw_image = None
+        self.preprocessed_image = None
+        self.out_image = None
+        return
 
-while (True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    def new_image(self, image):
+        self.raw_image = image
+        return
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
-    thresholded = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 2)
-    thresholded = cv2.bitwise_not(thresholded)
-    dilated = cv2.dilate(thresholded, kernel)
+    def preprocess(self):
+        gray = cv2.cvtColor(self.raw_image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+        thresholded = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 2)
+        thresholded = cv2.bitwise_not(thresholded)
+        self.preprocessed_image = cv2.dilate(thresholded, self.kernel)
+        return
 
-    # Display the resulting frame
-    cv2.imshow('frame', dilated)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    def search_table(self):
+        return
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print "Failed to open caption!"
+        sys.exit(1)
+
+    processor = ImageProcesser()
+
+    while (True):
+        # Capture frame-by-frame
+        _, frame = cap.read()
+
+        processor.new_image(frame)
+        processor.preprocess()
+
+        # Display the resulting frame
+        cv2.imshow('frame', processor.preprocessed_image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()

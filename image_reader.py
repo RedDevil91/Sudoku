ec16c2411a9cf2da7361c37173b21564acefba33
RedDevil91 <b.attila91@gmail.com>
2017-02-19 17:06:13 +0100

    Find Sudoku table on live image

--- image_reader.py
+++ image_reader.py
@@ -27,7 +27,23 @@ class ImageProcessor(object):
         return
 
     def search_table(self):
-        return
+        img, contours, hierarchy = cv2.findContours(self.preprocessed_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        blank_image = np.zeros((480, 640, 3), dtype=np.uint8)
+
+        max_c = None
+        max_mom = 0
+        for cont in contours:
+            perim = cv2.arcLength(cont, True)
+            approx = cv2.approxPolyDP(cont, 0.04 * perim, True)
+            if len(approx) == 4:
+                M = cv2.moments(cont)
+                if max_c is None or M > max_mom:
+                    max_c = cont
+                    max_mom = M
+        cv2.drawContours(blank_image, [max_c], -1, (255, 255, 255), -1)
+        gray = cv2.cvtColor(blank_image, cv2.COLOR_BGR2GRAY)
+        out = cv2.bitwise_and(self.raw_image, self.raw_image, mask=gray)
+        return out
 
 if __name__ == '__main__':
 
@@ -45,9 +61,10 @@ if __name__ == '__main__':
 
         processor.new_image(frame)
         processor.preprocess()
+        out = processor.search_table()
 
         # Display the resulting frame
-        cv2.imshow('frame', processor.preprocessed_image)
+        cv2.imshow('frame', out)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 

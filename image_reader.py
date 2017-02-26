e09e36c081d608d78ab81c51bef64364c6d3c34c
RedDevil91 <b.attila91@gmail.com>
2017-02-26 10:03:37 +0100

    Image perspective transformation with wrong corner sorting

--- image_reader.py
+++ image_reader.py
@@ -1,12 +1,38 @@
 import cv2
-import sys
 import numpy as np
+
+
+class ImageRoi(object):
+    def __init__(self, contour, moment, approx):
+        self.contour = contour
+        self.moment = moment
+        self.approx = np.reshape(approx, (4, 2)).astype(np.float32)
+        self.old_cord = np.float32([self.approx[1],
+                                    self.approx[0],
+                                    self.approx[2],
+                                    self.approx[3],])
+
+        min_point = (np.inf, np.inf)
+        max_point = (0, 0)
+        for point in self.approx:
+            if point[0] <= min_point[0] and point[1] <= min_point[1]:
+                min_point = point
+            elif point[0] >= max_point[0] and point[1] >= max_point[1]:
+                max_point = point
+        self.min_point = min_point
+        self.max_point = max_point
+        return
 
 
 class ImageProcessor(object):
     kernel = np.array([[0, 1, 0],
                        [1, 1, 1],
                        [0, 1, 0]], dtype=np.uint8)
+    roi_size = 150
+    new_cord = np.float32([[0,          0],
+                           [roi_size,   0],
+                           [0,          roi_size],
+                           [roi_size,   roi_size]])
 
     def __init__(self):
         self.raw_image = None
@@ -30,44 +56,68 @@ class ImageProcessor(object):
         img, contours, hierarchy = cv2.findContours(self.preprocessed_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         blank_image = np.zeros((480, 640, 3), dtype=np.uint8)
 
-        max_c = None
-        max_mom = 0
+        roi_img = None
         for cont in contours:
             perim = cv2.arcLength(cont, True)
             approx = cv2.approxPolyDP(cont, 0.04 * perim, True)
-            if len(approx) == 4:
+
+            contour_area = cv2.contourArea(cont)
+            hull_area = cv2.contourArea(cv2.convexHull(cont))
+            area_ratio = contour_area / float(hull_area)
+            if len(approx) == 4 and area_ratio > 0.9:
                 M = cv2.moments(cont)
-                if max_c is None or M > max_mom:
-                    max_c = cont
-                    max_mom = M
-        cv2.drawContours(blank_image, [max_c], -1, (255, 255, 255), -1)
+                if roi_img is None or M > roi_img.moment:
+                    roi_img = ImageRoi(cont, M, approx)
+
+        cv2.drawContours(blank_image, [roi_img.contour], -1, (255, 255, 255), -1)
         gray = cv2.cvtColor(blank_image, cv2.COLOR_BGR2GRAY)
-        out = cv2.bitwise_and(self.raw_image, self.raw_image, mask=gray)
+        masked_img = cv2.bitwise_and(self.raw_image, self.raw_image, mask=gray)
+        # cv2.drawContours(masked_img, [roi_img.contour], -1, (0, 255, 0), 1)
+
+        pers_matrix = cv2.getPerspectiveTransform(roi_img.old_cord, self.new_cord)
+        out = cv2.warpPerspective(masked_img, pers_matrix, (self.roi_size, self.roi_size))
         return out
 
 if __name__ == '__main__':
+    import time
+    import sys
+    import matplotlib.pyplot as plt
 
-    cap = cv2.VideoCapture(0)
+    input_img = cv2.imread('test_img.jpg')
 
-    if not cap.isOpened():
-        print "Failed to open caption!"
-        sys.exit(1)
-
+    start = time.time()
     processor = ImageProcessor()
+    processor.new_image(input_img)
+    processor.preprocess()
+    out = processor.search_table()
+    end = time.time()
+    print "Process time: %f" % (end-start)
 
-    while (True):
-        # Capture frame-by-frame
-        _, frame = cap.read()
+    plt.imshow(out)
+    plt.show()
 
-        processor.new_image(frame)
-        processor.preprocess()
-        out = processor.search_table()
-
-        # Display the resulting frame
-        cv2.imshow('frame', out)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+    # cap = cv2.VideoCapture(0)
+    #
+    # if not cap.isOpened():
+    #     print "Failed to open caption!"
+    #     sys.exit(1)
+    #
+    # processor = ImageProcessor()
+    #
+    # while (True):
+    #     # Capture frame-by-frame
+    #     _, frame = cap.read()
+    #
+    #     processor.new_image(frame)
+    #     processor.preprocess()
+    #     out = processor.search_table()
+    #
+    #     # Display the resulting frame
+    #     cv2.imshow('frame', out)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         cv2.imwrite('test_img.jpg', frame)
+    #         break
+    #
+    # # When everything done, release the capture
+    # cap.release()
+    # cv2.destroyAllWindows()

3a0cab1536d3683823b2aaf5af72d10a60022358
RedDevil91 <b.attila91@gmail.com>
2017-02-27 18:46:08 +0100

    Get numbers and draw grids

--- image_reader.py
+++ image_reader.py
@@ -14,6 +14,9 @@ class ImageRoi(object):
             self.old_cord[0], self.old_cord[1] = sorted_corners[1], sorted_corners[0]
         if sorted_corners[2, 0] > sorted_corners[3, 0]:
             self.old_cord[2], self.old_cord[3] = sorted_corners[3], sorted_corners[2]
+
+        # TODO: use the np.sum and np.diff to order the points! diff -> x-y
+        diff = np.diff(self.approx, axis=1)
         return
 
 
@@ -21,7 +24,7 @@ class ImageProcessor(object):
     kernel = np.array([[0, 1, 0],
                        [1, 1, 1],
                        [0, 1, 0]], dtype=np.uint8)
-    roi_size = 150
+    roi_size = 180
     new_cord = np.float32([[0,          0],
                            [roi_size,   0],
                            [0,          roi_size],
@@ -30,7 +33,8 @@ class ImageProcessor(object):
     def __init__(self):
         self.raw_image = None
         self.preprocessed_image = None
-        self.out_image = None
+        self.table_image = None
+        self.numbers = []
         return
 
     def new_image(self, image):
@@ -49,6 +53,9 @@ class ImageProcessor(object):
         img, contours, hierarchy = cv2.findContours(self.preprocessed_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         blank_image = np.zeros((480, 640, 3), dtype=np.uint8)
 
+        # TODO: use this sorting instead of the other!
+        cnts = sorted(contours, key=cv2.contourArea, reverse=True)
+
         roi_img = None
         for cont in contours:
             perim = cv2.arcLength(cont, True)
@@ -65,26 +72,36 @@ class ImageProcessor(object):
         cv2.drawContours(blank_image, [roi_img.contour], -1, (255, 255, 255), -1)
         gray = cv2.cvtColor(blank_image, cv2.COLOR_BGR2GRAY)
         masked_img = cv2.bitwise_and(self.raw_image, self.raw_image, mask=gray)
-        # cv2.drawContours(masked_img, [roi_img.contour], -1, (0, 255, 0), 1)
+        masked_img = cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY)
 
         pers_matrix = cv2.getPerspectiveTransform(roi_img.old_cord, self.new_cord)
-        out = cv2.warpPerspective(masked_img, pers_matrix, (self.roi_size, self.roi_size))
-        self.drawGrid(out)
-        return out
+        self.table_image = cv2.warpPerspective(masked_img, pers_matrix, (self.roi_size, self.roi_size))
+        self.getNumbers()
+        return self.table_image
 
     def drawGrid(self, img):
-        cv2.line(img, (50, 0),  (50, 150),  (255, 0, 0), 1)
-        cv2.line(img, (100, 0), (100, 150), (255, 0, 0), 1)
-        cv2.line(img, (0, 50),  (150, 50),  (255, 0, 0), 1)
-        cv2.line(img, (0, 100), (150, 100), (255, 0, 0), 1)
+        cv2.line(img, (self.roi_size/3, 0),   (self.roi_size/3, self.roi_size),     (255, 0, 0), 1)
+        cv2.line(img, (2*self.roi_size/3, 0), (2*self.roi_size/3, self.roi_size),   (255, 0, 0), 1)
+        cv2.line(img, (0, self.roi_size/3),   (self.roi_size, self.roi_size/3),     (255, 0, 0), 1)
+        cv2.line(img, (0, 2*self.roi_size/3), (self.roi_size, 2*self.roi_size/3),   (255, 0, 0), 1)
+        return
 
+    def getNumbers(self):
+        for row in range(9):
+            for col in range(9):
+                number = self.table_image[row * self.roi_size / 9:(row + 1) * self.roi_size / 9,
+                                          col * self.roi_size / 9:(col + 1) * self.roi_size / 9]
+                _, number = cv2.threshold(number, 110, 255, cv2.THRESH_BINARY_INV)
+                self.numbers.append(number)
+        return
 
 if __name__ == '__main__':
     import time
     import sys
     import matplotlib.pyplot as plt
 
-    input_img = cv2.imread('test_img.jpg')
+    # ****************************************************************
+    input_img = cv2.imread('test_img2.jpg')
 
     start = time.time()
     processor = ImageProcessor()
@@ -96,6 +113,9 @@ if __name__ == '__main__':
 
     plt.imshow(out)
     plt.show()
+    for idx, number in enumerate(processor.numbers):
+        cv2.imwrite('numbers/numbers%d.jpg' % idx, number)
+    # ****************************************************************
 
     # cap = cv2.VideoCapture(0)
     #
@@ -115,9 +135,13 @@ if __name__ == '__main__':
     #
     #     # Display the resulting frame
     #     cv2.imshow('frame', out)
-    #     if cv2.waitKey(1) & 0xFF == ord('q'):
-    #         cv2.imwrite('test_img.jpg', frame)
+    #
+    #     key = cv2.waitKey(1) & 0xFF
+    #     if key == ord('q'):
     #         break
+    #     elif key == ord('c'):
+    #         import glob
+    #         cv2.imwrite('test_img%d.jpg' % len(glob.glob('*.jpg')), frame)
     #
     # # When everything done, release the capture
     # cap.release()

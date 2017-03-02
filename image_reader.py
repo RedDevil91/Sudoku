2ab4d3354ee81137744b30095902dfdf7b3fb1d8
RedDevil91 <b.attila91@gmail.com>
2017-03-02 21:31:56 +0100

    code refactor, vertical and horizontal line detect

--- image_reader.py
+++ image_reader.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-class ImageRoi(object):
+class RegionOfInterest(object):
     def __init__(self, contour, moment, approx):
         self.contour = contour
         self.moment = moment
@@ -23,7 +23,10 @@ class ImageProcessor(object):
     kernel = np.array([[0, 1, 0],
                        [1, 1, 1],
                        [0, 1, 0]], dtype=np.uint8)
-    roi_size = 27 * 9
+    horizontal_kernel = np.ones((1, 50), dtype=np.uint8)
+    vertical_kernel = np.ones((50, 1), dtype=np.uint8)
+
+    roi_size = 28 * 9
     corners = np.float32([[0,          0],
                           [roi_size,   0],
                           [roi_size,   roi_size],
@@ -43,7 +46,7 @@ class ImageProcessor(object):
         return
 
     def preProcessImage(self, image):
-        gray = cv2.cvtColor(self.raw_image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (11, 11), 0)
         thresholded = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 5, 2)
         return cv2.dilate(thresholded, self.kernel)
@@ -53,8 +56,42 @@ class ImageProcessor(object):
 
         pers_matrix = cv2.getPerspectiveTransform(roi.corners, self.corners)
         self.table_image = cv2.warpPerspective(self.raw_image, pers_matrix, (self.roi_size, self.roi_size))
-        self.table_image = cv2.cvtColor(self.table_image, cv2.COLOR_BGR2GRAY)
-        self.getNumbers()
+
+        # cv2.imshow('orig', self.table_image)
+
+        image = cv2.cvtColor(self.table_image, cv2.COLOR_BGR2GRAY)
+        image = cv2.adaptiveThreshold(image, 255,
+                                      cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, -2)
+
+        vertical = cv2.erode(image, self.horizontal_kernel)
+        vertical = cv2.dilate(vertical, self.horizontal_kernel)
+        # vertical = cv2.dilate(vertical, self.horizontal_kernel)
+
+        cv2.imshow('vertical', vertical)
+
+        horizontal = cv2.erode(image, self.vertical_kernel)
+        horizontal = cv2.dilate(horizontal, self.vertical_kernel)
+        # horizontal = cv2.dilate(horizontal, self.vertical_kernel)
+
+        cv2.imshow('horizontal', horizontal)
+
+        image = cv2.bitwise_and(vertical, horizontal)
+
+        # cv2.imshow('points', self.table_image)
+
+        img, contours, hierarchy = cv2.findContours(image.copy(),
+                                                    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cont in contours:
+            mom = cv2.moments(cont)
+            try:
+                x, y = int(mom['m10']/mom['m00']), int(mom['m01']/mom['m00'])
+                cv2.circle(self.table_image, (x, y), 4, (0, 255, 0), -1)
+            except ZeroDivisionError:
+                print 'ZeroError!'
+
+        # self.getNumbers()
+        # self.drawGrid(self.table_image)
         return self.table_image
 
     def getSquares(self, image):
@@ -72,7 +109,7 @@ class ImageProcessor(object):
             area_ratio = contour_area / float(hull_area)
             if len(approx) == 4 and area_ratio > 0.9:
                 M = cv2.moments(cont)
-                roi_img = ImageRoi(cont, M, approx)
+                roi_img = RegionOfInterest(cont, M, approx)
                 break
         else:
             # if there is no square
@@ -102,7 +139,6 @@ class ImageProcessor(object):
 if __name__ == '__main__':
     import time
     import sys
-    import matplotlib.pyplot as plt
 
     def load_from_file(filename):
         input_img = cv2.imread(filename)
@@ -113,8 +149,8 @@ if __name__ == '__main__':
         end = time.time()
         print "Process time: %f" % (end-start)
 
-        plt.imshow(processor.table_image)
-        plt.show()
+        cv2.imshow('Table', processor.table_image)
+        cv2.waitKey()
         for idx, number in enumerate(processor.numbers):
             cv2.imwrite('numbers/numbers%d.jpg' % idx, number)
 
@@ -147,5 +183,5 @@ if __name__ == '__main__':
         cap.release()
         cv2.destroyAllWindows()
 
-    load_from_file('test_img2.jpg')
+    load_from_file('test_img0.jpg')
     # load_from_camera(0)
